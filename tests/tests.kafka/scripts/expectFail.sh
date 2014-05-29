75481ec662ebaf15355788127c2876c05540d554
Rohit Wagle <ro.wagle@gmail.com>
2014-05-28 21:06:32 -0400

    added a basic function test #30

---
+++ tests/tests.kafka/scripts/expectFail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# *******************************************************************************
+# * Copyright (C)2014, International Business Machines Corporation and *
+# * others. All Rights Reserved. *
+# *******************************************************************************
+#
+
+ex=$@
+
+if [ -z "$ex" ]; then
+		echo "No executable found"
+		exit 1
+fi
+
+$ex
+ret=$?
+
+if [ $ret -eq 0 ]; then
+		echo "ERROR: Test Failed. Command passed but was expected to fail"
+		exit 1
+fi
